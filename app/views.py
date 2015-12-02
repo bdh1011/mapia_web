914c7d4d9982389eb11a0e9cf8079768f32b3eb4
Yoon Han <hyku2014@gmail.com>
2015-12-02 18:14:28 +0900

    route
    href modification.

--- app/views.py
+++ app/views.py
@@ -10,39 +10,35 @@ from werkzeug.security import check_password_hash, generate_password_hash
 import logging
 import json
 
+@app.route('/', methods=['GET'])
+def enter():
+	return render_template('enter.html')
+
 
 @app.route('/login', methods=['GET'])
 def login():
 	return render_template('login.html')
 
 
-
-@app.route('/enter', methods=['GET'])
-def enter():
-	return render_template('enter.html')
-
-	
-
-
 @app.route('/read', methods=['GET'])
 def read():
 	return render_template('read.html')
 
+<<<<<<< Updated upstream
 	
 
 @app.route('/tmp', methods=['GET'])
 def tmp():
 	return render_template('tmp.html')
 
+=======
+>>>>>>> Stashed changes
 
 @app.route('/register', methods=['GET'])
 def register():
 	return render_template('register.html')
 
-	
 
 @app.route('/comment', methods=['GET'])
 def comment():
 	return render_template('comment.html')
-
-	
